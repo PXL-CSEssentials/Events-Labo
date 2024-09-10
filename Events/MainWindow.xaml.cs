6c2ad14f631c8c93a6dd5253bcfc751b8cadb98d
Wim Bervoets <wim.bervoets@pxl.be>
2024-09-10 20:46:11 +0200

    final

---
+++ Events/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Events;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void showMessageButton_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("De naam van de knop is: " + ((Button)sender).Name, "Button clicked!");
+    }
+
+    private void nameTextBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        nameOutputLabel.Content = "The key that was pressed is: " + e.Key;
+
+        if(e.Key >= Key.F1 && e.Key <= Key.F12)
+        {
+            nameTextBox.Background = GetRandomColorBrush();
+        }
+
+        if (e.Key == Key.Return)
+        {
+            MessageBox.Show(nameTextBox.Text, "Enter pressed!");
+            e.Handled = true;
+        }
+    }
+
+    private void original_KeyDown(object sender, KeyEventArgs e)
+    {
+
+        if(e.KeyboardDevice.Modifiers == ModifierKeys.Shift && e.Key >= Key.A && e.Key <= Key.Z)
+        {
+            convertedLabel.Content += e.Key.ToString().ToLower();
+        }
+        else if(e.KeyboardDevice.Modifiers == ModifierKeys.None && e.Key >= Key.A && e.Key <= Key.Z)
+        {
+            convertedLabel.Content += e.Key.ToString().ToUpper();
+        }
+    }
+
+    private void counterButton_Click(object sender, RoutedEventArgs e)
+    {
+        Button button = (Button)sender;
+        int counter = int.Parse(button.Content.ToString());
+        button.Content = counter + 1;
+    }
+
+    Random random = new Random();
+    //Returns a random color every time it is called
+    private Brush GetRandomColorBrush()
+    {
+        return new SolidColorBrush(Color.FromRgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255)));
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Events-Labo
+# Labo 5 - Events
+
+## Deel 1
+- Maak een event procedure voor het click event van de *showMessageButton*
+- Toon een *MessageBox* met de tekst "Er werd op de knop geklikt"
+	``` 
+	MessageBox.Show("Er werd op de knop geklikt"); 
+	```
+
+- Zorg ervoor dat dezelfde event procedure wordt uitgevoerd wanneer er op de *showAgainButton* geklikt wordt
+- Wijzig nu de tekst van de MessageBox zodat de naam van de knop waar op geklikt werd wordt weergegeven als volgt:
+
+	![Afbeelding](media/button_clicked.png)
+
+	Gebruik slechts één event procedure voor beide knoppen!
+	> Tip: Gebruik de *sender* parameter van de event procedure
+
+## Deel 2
+- Maak een event procedure voor het *KeyDown* event van de *nameTextBox*
+- Toon het karakter dat werd ingegeven in de *nameTextBox* in het *nameOutputLabel*
+
+	![Afbeelding](media/textbox_keydown.png)
+
+- Wijzig de achtergrondkleur van de *nameTextBox* wanneer één van de functietoetsen wordt ingedrukt (F1 - F12), gebruik hiervoor de **reeds bestaande** functie *GetRandomColorBrush()*
+	> Tip: Gebruik *Key* eigenschap van de *e* parameter van de event procedure
+- Toon nu een MessageBox met de volledige naam wanneer de *Enter* toets wordt ingedrukt
+	
+	![Afbeelding](media/textbox_enter.png)
+
+## Deel 3
+- Zorg ervoor dat alle alfanumerische (A-Z) karakters die worden ingegeven in de *originalTextBox* worden omgezet naar hoofdletters of omgekeerd. Toon de output in het *convertedLabel*
+	
+	![Afbeelding](media/keydown_modifiers.png)
+
+## Deel 4
+- Maak **één** event procedure met de naam *counterButton_Click* en zorg dat deze wordt uitgevoerd bij het click-event van de 3 buttons: *firstButton*, *secondButton* en *thirdButton**
+- Bepaal in de event procedure welke knop werd ingedrukt
+- Verhoog het nummer wat wordt weergegeven als tekst van de knop met 1
+
+![Afbeelding](media/counterbuttons.png)
